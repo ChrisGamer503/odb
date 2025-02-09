9f584120079ed482e5fb24191996966e23a680c8
Alberto Alas <oa3973392@gmail.com>
2025-02-09 11:18:25 -0600

    DB_connection added

--- src/index.js
+++ src/index.js
@@ -3,7 +3,24 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const morgan = require('morgan');
+const sql = require('mssql');
+const config = require('./database/DB_connection');
 require('dotenv').config();
+
+//function for the connection to the database
+async function DB_test() {
+    try {
+        const pool = await sql.connect(config);
+        console.log('Conexión exitosa');
+        await pool.close();
+    } catch (error) {
+        console.log("error al conectar a sql server: ", error);
+    }
+}
+
+//check if the connection to the database is successful
+DB_test();
+
 
 //settings
 const port = process.env.PORT || 3002;
@@ -20,6 +37,5 @@ app.use(require('./routes/index'));
 
 //starting the server
 app.listen(port, () => {
-    console.log('Conexión exitosa a la base de datos');
     console.log('Server listening on port ' + port)
 })
